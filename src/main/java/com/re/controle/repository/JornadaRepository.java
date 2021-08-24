555599b96d26cad459524b254659c4e9ef19b9fb
Erasmo Frota <jose.erasmo@uece.br>
2021-08-24 19:41:33 -0300

    Initial commit

---
+++ src/main/java/com/re/controle/repository/JornadaRepository.java
@@ -0,0 +1,9 @@
+package com.re.controle.repository;
+
+import com.re.controle.model.JornadaTrabalho;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface JornadaRepository extends JpaRepository<JornadaTrabalho, Long> {
+}
